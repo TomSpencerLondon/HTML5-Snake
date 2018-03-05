1930fbc5a0698a729401e4d7962848610fc508b0
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-03-05 17:19:18 +0000

    Add game object

--- game.js
+++ game.js
@@ -1,2 +1,58 @@
 var canvas = document.getElementById("the-game");
 var context = canvas.getContext("2d");
+
+game = {
+
+  score: 0,
+  fps: 8,
+  over: false,
+  message: null,
+
+  start: function() {
+    game.over = false;
+    game.message = null;
+    game.score = 0;
+    game.fps = 8;
+    snake.init();
+    food.set();
+  },
+
+  stop: function() {
+    game.over = true;
+    game.message = 'GAME OVER - PRESS SPACEBAR';
+  },
+
+  drawBox: function(x, y, size, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.moveTo(x - (size / 2), y - (size / 2));
+    context.lineTo(x + (size / 2), y - (size / 2));
+    context.lineTo(x + (size / 2), y + (size / 2));
+    context.lineTo(x - (size / 2), y + (size / 2));
+    context.closePath();
+    context.fill();
+  },
+
+  drawScore: function() {
+    context.fillStyle = '#999';
+    context.font = (canvas.height) + 'px Impact, sans-serif';
+    context.textAlign = 'center';
+    context.fillText(game.score, canvas.width/2, canvas.height  * .9);
+  },
+
+  drawMessage: function() {
+    if (game.message !== null) {
+      context.fillStyle = '#00F';
+      context.strokeStyle = '#FFF';
+      context.font = (canvas.height / 10) + 'px Impact';
+      context.textAlign = 'center';
+      context.fillText(game.message, canvas.width/2, canvas.height/2);
+      context.strokeText(game.message, canvas.width/2, canvas.height/2);
+    }
+  },
+
+  resetCanvas: function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+};
