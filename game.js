ab6ca76e5fa4c3546c4e87e227a2dce00b35480e
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-03-05 17:22:54 +0000

    Add helper methods

--- game.js
+++ game.js
@@ -155,3 +155,26 @@ food = {
 
 };
 
+inverseDirection = {
+  'up':'down',
+  'left':'right',
+  'right':'left',
+  'down':'up'
+};
+
+keys = {
+  up: [38, 75, 87],
+  down: [40, 74, 83],
+  left: [37, 65, 72],
+  right: [39, 68, 76],
+  start_game: [13, 32]
+};
+
+Object.prototype.getKey = function(value){
+  for(var key in this){
+    if(this[key] instanceof Array && this[key].indexOf(value) >= 0){
+      return key;
+    }
+  }
+  return null;
+};
